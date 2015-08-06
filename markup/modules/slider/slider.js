75fdf9a29dc0b51da8abb7005f010b3684bf4759
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-08-06 13:12:07 +0500

    fix

---
+++ markup/modules/slider/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#slider').slick({
+        speed: 1300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        arrows: false,
+        draggable: true
+    });
+});
