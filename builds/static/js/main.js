2e36b59232e01382a822a866fb446f659458fbe3
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-08-11 12:00:51 +0500

    new pages

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -1,3 +1,12 @@
+jQuery(document).ready(function ($) {
+    $('.catalog-tabs__item').eq(0).addClass('active');
+    $('.catalog-tabs-content-box').eq(0).addClass('active');
+    $('.catalog-tabs__item').click(function () {
+        var itemCount = $(this).index();
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.catalog-tabs-content').find('.catalog-tabs-content-box').eq(itemCount).addClass('active').siblings().removeClass('active');
+    });
+});
 $(document).ready(function () {
     $('#slider').slick({
         speed: 1300,
